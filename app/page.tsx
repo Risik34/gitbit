3d9e6fa67282a76ee8cd2b1b2c3db0394958cb83
risik34 <risimn7@gmail.com>
2024-11-22 20:26:01 +0530

    feat: Adds prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 import React from "react";
 
-export default function home() {
-  return <Button>Gitbit</Button>;
+export default async function home() {
+  const userData=await prisma.user.findMany()
+  return (
+  <div>
+      {userData.map(user=><p key={user.email}>{user.name} {user.email}</p>)}
+      <Button>Hello</Button>
+    </div>
+  )
 }
